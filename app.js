596c23eb316c36699ecfeb8f34c66127509f5b96
Danishhameed505 <155366241+Danishhameed505@users.noreply.github.com>
2024-03-08 19:46:36 +0500

    chapter-05-JS
    chapter-05-JS

---
+++ app.js
@@ -0,0 +1,31 @@
+// Q # 01 Write a program that take two numbers & add them in a new variable. Show the result in your browser.  
+// Ans:
+let a = 3;
+let b = 5;
+let sum = a + b;
+console.log("sum of",a ,"and", b, "is", sum )
+
+
+// Q # 02 Repeat task1 for subtraction, multiplication, division & modulus.
+// Ans:
+let a1 = 3;
+let b1 = 5;
+let sub = a1 - b1;
+console.log("sub of",a1 ,"and", b1, "is", sub );
+
+let a2 = 3;
+let b2 = 5;
+let multi = a2 * b2;
+console.log("multi of",a2 ,"and", b2, "is", multi );
+
+let a3 = 3;
+let b3 = 5;
+let divd = a3 / b3;
+console.log("divd of",a3 ,"and", b3, "is", divd );
+
+let num1 = 10;
+let num2 = 3;
+let remainder = num1 % num2;
+console.log("remainder of",num1 ,"and", num2, "is",remainder);
+
+// Q # 03 Do the following using JS Mathematic Expressions a. Declare a variable.
